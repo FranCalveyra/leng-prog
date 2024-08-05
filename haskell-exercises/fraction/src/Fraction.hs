02cefd2863030a610e4680873443dad9ba63fe38
Francisco Calveyra <franciscocalveyra24@gmail.com>
2024-08-05 15:48:08 -0300

    testing remote change

--- haskell-exercises/fraction/src/Fraction.hs
+++ haskell-exercises/fraction/src/Fraction.hs
@@ -4,23 +4,43 @@ type Fraction = (Int, Int)
 
 -- Implement the `add` Function
 
+first :: (Int, Int) -> Int
+first (x,_) = x
+
+second :: (Int, Int) -> Int
+second (_,x) = x
+
+
 add :: Fraction -> Fraction -> Fraction
-add n d = error "Implement it"
+add a b = (num, den)
+   where
+     num = first(a)*second(b) + first(b)*second(a)
+     den = second(a)*second(b)
 
 -- Implement the `sub` Function
 
 sub :: Fraction -> Fraction -> Fraction
-sub n d = error "Implement it"
+sub a b = (num, den)
+     where
+       num = first(a)*second(b) - first(b)*second(a)
+       den = second(a)*second(b)
 
 -- Implement the `mul` Function
 
 mul :: Fraction -> Fraction -> Fraction
-mul n d = error "Implement it"
+mul a b = (num, den)
+  where
+    num = first(a)*first(b)
+    den = second(a)*second(b)
 
 -- Implement the `divide` Function
 
 divide :: Fraction -> Fraction -> Fraction
-divide n d = error "Implement it"
+divide a b
+  | first(b) == 0 = error "Cannot divide by zero"
+  | otherwise = mul a invertedB
+    where
+    invertedB = (second(b), first(b))
 
 -- Implement the `hcf` Function
 
