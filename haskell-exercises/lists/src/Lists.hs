08ad6e9d83b64d0fe17277a61a04e122c41e72c3
Francisco Calveyra <franciscocalveyra24@gmail.com>
2024-08-12 15:09:04 -0300

    TP2 Finished

--- haskell-exercises/lists/src/Lists.hs
+++ haskell-exercises/lists/src/Lists.hs
@@ -1,6 +1,6 @@
 module Lists (member, union, intersection, difference,
               insert, insertionSort, firsts,
-              binaryToDecimal, toDecimal, toDec, decimal,
+              binaryToDecimal, toDecimal, toDec, decimal, firsts,
               binaryAdd) where
   
 import Data.Char(digitToInt)  
@@ -17,36 +17,57 @@ union (x:xs) ys
   | otherwise   = x : union xs ys
 
 intersection:: [Int] -> [Int] -> [Int]
-intersection = error "Implement it"
+intersection [] _ = []
+intersection _ [] = []
+intersection l1 l2 = [a | a <- l1, member a l2]
+
 
 difference:: [Int] -> [Int] -> [Int]
-difference  = error "Implement it"
+difference [] a = []
+difference a [] = a
+difference l1 l2 = [a | a <- l1, not (member a l2)]
 
 insert:: Int -> [Int] -> [Int]
-insert = error "Implement it"
+insert b [] = [b]
+insert b (x:xs)
+      | b < x = b :(x:xs)
+      | otherwise = x: (insert b xs)
 
 insertionSort :: [Int] -> [Int]
-insertionSort = error "Implement it"
+insertionSort [] = []
+insertionSort (x:xs) = insert x (insertionSort xs)
 
 binaryToDecimal :: [Int] -> Int
-binaryToDecimal = error "Implement it"
+binaryToDecimal [] = 0
+binaryToDecimal list = sum [x*2^i | (i, x) <- zip [0..] revList]
+    where
+      revList = reverse list
     
 toDecimal :: Int -> [Int] -> Int
-toDecimal = error "Implement it"
-    
+toDecimal _ [] = 0
+toDecimal base list = sum [x*base^i | (i, x) <- zip [0..] revList]
+    where
+      revList = reverse list
+
 toDec::Int -> String -> Int
-toDec base s = error "Implement it"
+toDec base "" = 0
+toDec base s = sum [(digitToInt x)*base^i | (i, x) <- zip [0..] revList]
+                   where
+                     revList = reverse s
 
 -- Same as `toDec` But use a list comprehension
 
 decimal::Int -> String -> Int
-decimal  = error "Implement it"
+decimal base "" = 0
+decimal base s = sum [(digitToInt x)*base^i | (i, x) <- zip [0..] (reverse s)]
 
 firsts::[a] -> [[a]]
-firsts = error "Implement it"
+firsts [] = []
+firsts list = [take i list | i <- [1..length list] ]
 
 -- Given two String that represents numbers in binary implement the 'binaryAdd' function
 -- DO NOT USE a predefined '+' operation
 
 binaryAdd::String -> String -> String
 binaryAdd  = error "Implement it"
+-- It's optional, so I ain't gonna do it :)
