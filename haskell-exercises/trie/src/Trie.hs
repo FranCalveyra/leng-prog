c575b41f54109b90f027b61ab020de2abf680991
FranCalveyra <franciscocalveyra24@gmail.com>
2024-08-21 13:02:50 -0300

    Decode and toList don't work

--- haskell-exercises/trie/src/Trie.hs
+++ haskell-exercises/trie/src/Trie.hs
@@ -2,19 +2,35 @@ module Trie  (Trie(..), left, right, find, decode, toList) where
 
 import Bit
   
-data Trie a = Complete -- Add the Type definition deriving (Eq, Show)
+--Trie a === Trie<T> in Java
+data Trie a = Leaf a | (Trie a) :-: (Trie a) | Nil deriving (Show, Eq)
+-- Add the Type definition deriving (Eq, Show)
+
+
+--Should it be Trie a or Trie Char??? IDK
             
 left::Trie a -> Trie a
-left = error "Define it"
+left Nil = error "Exception: Left of Nil"
+left (l :-: _) = l
 
 right::Trie a -> Trie a
-left = error "Define it"
-  
+right Nil = error "Exception: Right of Nil"
+right (_ :-: r) = r
+
 find::Bits -> Trie a -> a
-find = error "Define it"
+find [] _ = error "Trying to find nothing"
+find _ Nil = error "Trying to find something on a null trie"
+find bits trie = recFind 0 bits trie
+  where
+     recFind _ _ (Leaf x) = x
+     recFind index b t = if b!!index == F then recFind (index + 1) b (left t) else recFind (index + 1) b (right t)
 
 decode::Bits -> Trie Char -> String
-decode = error "Define it"
+decode [] _ = ""
+decode _ Nil = error "Trying to decode a null trie"
+decode bits trie = [find (take i bits) root | i<-[0..]]
+  where
+     root = trie
 
 toList::Trie a -> [(a, Bits)]
 toList = error "Define it"
