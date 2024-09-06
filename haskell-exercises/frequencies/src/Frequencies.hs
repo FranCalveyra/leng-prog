5e80e9d92312430a7a8f1001fcc957ffc3faf823
FranCalveyra <franciscocalveyra24@gmail.com>
2024-09-06 17:17:24 -0300

    Wanted to create new module, but can't run it

--- haskell-exercises/frequencies/src/Frequencies.hs
+++ haskell-exercises/frequencies/src/Frequencies.hs
@@ -1,5 +1,4 @@
-module Frequencies  (Frequency, frequencyMap, frequencies, insert, insertionSort,
- encode, encode2, Student(..), Element(..), averageScores, student, scoreAsString, printAverages) where
+module Frequencies  (Frequency, frequencyMap, frequencies, insert, insertionSort) where
 
 import Data.Map(Map)
 import qualified Data.Map as Map
@@ -46,46 +45,3 @@ mergesort list = merge (mergesort firstHalf) (mergesort secondHalf)
   where
     (firstHalf, secondHalf) = splitAt mid list
     mid = (length list ) `div` 2
-
--- Add exercises from MidTermHaskell2022
-encode:: String -> [(Int, Char)]
-encode ""     = []
-encode (x:xs) = encode' 1 x xs
-  where
-    encode' n c "" = [(n,c)]
-    encode' n c (y:ys)
-      | c == y = encode' (n+1) c ys
-      | otherwise = [(n,c)] ++ (encode' 1 y ys)
-
-data Element = One Char | Repeat Char Int deriving (Show, Eq)
-
-encode2::String -> [Element]
-encode2 l = [if n == 1 then One char else Repeat char n | (n,char) <- encode l]
-
-
--- AVERAGE SCORES
-data Student = Student{idNumber::Int, name::String}
-
-student:: Int -> String -> Student
-student id newName = Student{idNumber=id, name=newName}
-
-averageScores:: [(Int, Int)] -> Map.Map Int Double
-averageScores [] = Map.empty
-averageScores list = Map.fromList (map calculateAverages (buildScoreMap list))
-  where
-    calculateAverages (id, scores) = (id, fromIntegral (sum scores) / fromIntegral (length scores))
-    buildScoreMap [] = []
-    buildScoreMap ((id, score):xs) = (id, scores id ((id, score):xs)) : buildScoreMap (filter ((/= id) . fst) xs)
-    scores id ys = map snd (filter ((== id) . fst) ys)
-
-scoreAsString::Int -> Map Int Double -> String
-scoreAsString id map = toString $ Map.lookup id map
-  where
-    toString Nothing = "No Show"
-    toString (Just x) = show x
-
-
-printAverages::[Student] -> Map Int Double -> IO ()
-printAverages (x:xs) scores = do
-  putStrLn (name x ++ " " ++ (scoreAsString (idNumber x) scores))
-  printAverages xs scores
