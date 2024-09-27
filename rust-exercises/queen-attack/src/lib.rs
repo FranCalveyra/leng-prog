7e73437aba69718b5bf7ffbe3ca2b82bda391dc1
FranCalveyra <franciscocalveyra24@gmail.com>
2024-09-26 23:14:45 -0300

    Finished Queen Attack

--- rust-exercises/queen-attack/src/lib.rs
+++ rust-exercises/queen-attack/src/lib.rs
@@ -8,16 +8,23 @@ pub struct Queen {
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        panic!("Implement me");
+        if rank < 0 || file < 0 || rank >=8 || file >=8 {return None};
+        return Some(ChessPosition(rank, file));
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        panic!("Implement me");
+        Queen {position: position}
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-    panic!("Implement me");
-}
+    let same_row = self.position.0 == other.position.0;
+    let same_col = self.position.1 == other.position.1;
+
+    let delta_col = (self.position.1 - other.position.1).abs();
+    let delta_row = (self.position.0 - other.position.0).abs();
+
+    return same_row || same_col || delta_col == delta_row;
+    }   
 }
