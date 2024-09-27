607196a12a18c4b4e1b854a52ea20f5e677bf93c
FranCalveyra <franciscocalveyra24@gmail.com>
2024-09-26 23:30:10 -0300

    Clock in development

--- rust-exercises/clock/src/lib.rs
+++ rust-exercises/clock/src/lib.rs
@@ -8,16 +8,32 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        panic!("Implement me!");
+        Self {hours: hours, minutes: minutes}
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        panic!("Implement me!");
+        let mut new_minutes = self.minutes + minutes;
+        let mut hours = 0; 
+        
+        if new_minutes < 0{
+            let whole_minutes = (self.hours * 60) + new_minutes;
+            hours = whole_minutes / 60;
+            new_minutes = whole_minutes % 60;
+        }
+
+        if new_minutes >=60{
+            hours = new_minutes / 60;
+            new_minutes = new_minutes % 60;
+        }
+        
+        let result_hours = if hours != 0 {self.hours + hours} else {self.hours};
+        
+        Self::new(result_hours, new_minutes)
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        panic!("Implement me!");
+        write!(f, "{}:{}", self.hours, self.minutes)
     }
 }
