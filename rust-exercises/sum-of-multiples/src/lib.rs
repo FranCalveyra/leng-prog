237b536478b2acc9608f623493828e3074ad0feb
FranCalveyra <franciscocalveyra24@gmail.com>
2024-10-07 17:26:26 -0300

    Finished Sum of Multiples

--- rust-exercises/sum-of-multiples/src/lib.rs
+++ rust-exercises/sum-of-multiples/src/lib.rs
@@ -1,3 +1,27 @@
+use std::collections::HashSet;
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    0
+    let mut nums = HashSet::new();
+    if(factors.is_empty() || limit == 0){
+        return 0;
+    }
+
+
+    for x in factors[0]..limit {
+        if isMultipleOfAny(x, factors){
+            nums.insert(x);
+        }
+    }
+
+    nums.iter().sum::<u32>()
+}
+
+fn isMultipleOfAny(x :u32, factors: &[u32]) -> bool {
+    for n in factors{
+        if *n == 0 {return false;}
+        if x % n == 0{
+            return true;
+        }
+    }
+    false
 }
