5ae2ce775c3609f159d0de6fa2481f774ec84692
FranCalveyra <franciscocalveyra24@gmail.com>
2024-10-07 16:57:04 -0300

    All Clock tests pass

--- rust-exercises/clock/src/lib.rs
+++ rust-exercises/clock/src/lib.rs
@@ -8,46 +8,22 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let clock = Self {hours:0, minutes:0};
-        clock.add_minutes((hours * 60) + minutes)
+        let clock = Self {hours:24, minutes:0};
+        clock.add_minutes(hours * 60 + minutes)
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
         let total_minutes = self.hours * 60 + self.minutes + minutes;
-        let mut hours = 0;
-        let mut result_minutes = 0;
-
-        if self.hours == 0{
-            let additional = if total_minutes < 0 {24 * 60} else {0};
-            let final_time = additional + total_minutes;
-            hours = final_time / 60;
-            result_minutes = final_time % 60;
-        }
-        else{
-            hours = total_minutes / 60;
-            result_minutes = total_minutes % 60; 
-        }
-
-        while hours < 0 {
-            hours += 24;
-        }
-
-        while result_minutes < 0 {
-            result_minutes +=60;
-        }
-
-        if hours >= 24{
-            hours = hours % 24;
-        }
-
-        if result_minutes >= 60{
-            hours += result_minutes / 60;
-            result_minutes = result_minutes % 60;
-        }
-
+        let mut mins = total_minutes % (24 * 60);
         
+        if mins < 0{
+            mins += 24 * 60
+        }
 
-        Self {hours: hours, minutes: result_minutes} 
+        let hours = mins / 60;
+        let res_mins = mins % 60;
+        
+        Self { hours: hours, minutes: res_mins } 
     }
 }
 
