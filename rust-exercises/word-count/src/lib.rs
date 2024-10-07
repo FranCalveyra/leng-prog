d48678e8f4d40cceabb0215eb55e41d6b32370b8
FranCalveyra <franciscocalveyra24@gmail.com>
2024-10-07 18:01:57 -0300

    Finished word count

--- rust-exercises/word-count/src/lib.rs
+++ rust-exercises/word-count/src/lib.rs
@@ -2,5 +2,13 @@ use std::collections::HashMap;
 
 /// Count occurrences of words.
 pub fn word_count(phrase: &str) -> HashMap<String, u32> {
-  panic!("Implement me!")
+  let words: Vec<&str> = phrase.split_whitespace().collect::<Vec<&str>>();
+  let mut map: HashMap<String, u32> = HashMap::new();
+  
+  for word in words{
+    let w = word.to_string().to_lowercase();     
+    map.insert(w.clone(), map.get(&w).unwrap_or(&0) + 1);
+  }
+  
+  map
 }
