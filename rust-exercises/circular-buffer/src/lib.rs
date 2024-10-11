e6d687be2a083d3a82d49e59688f5235a8db5d73
FranCalveyra <franciscocalveyra24@gmail.com>
2024-10-11 12:15:17 -0300

    Finished Circular Buffer

--- rust-exercises/circular-buffer/src/lib.rs
+++ rust-exercises/circular-buffer/src/lib.rs
@@ -8,6 +8,7 @@ pub struct CircularBuffer<T:Clone> {
     buffer: Vec<Option<T>>,
     size: usize,
     next: usize,
+    read: usize,
 }
 
 impl<T:Clone> CircularBuffer<T> {
@@ -16,26 +17,54 @@ impl<T:Clone> CircularBuffer<T> {
             buffer: vec![None; size],
             size: 0,
             next: 0,
+            read: 0,
         }
     }
 
     pub fn read(&mut self) -> Result<T, Error> {
-        todo!()
+        if self.is_empty() {return Err(Error::EmptyBuffer);}
+        let elem = self.buffer[self.read].clone();
+        self.buffer[self.read] = None;
+        self.read = self.alter_next_index(self.read);
+        println!("Current value of read index: {}", self.read);
+
+
+        self.size -=1; 
+        Ok(elem.as_ref().unwrap().clone())
+
     }
 
     pub fn write(&mut self, byte: T) -> Result<(), Error> {
-        todo!()
+        if self.is_full() {return Err(Error::FullBuffer);}
+        self.buffer[self.next] = Some(byte);
+        self.next = self.alter_next_index(self.next);
+        println!("Current write index value: {}", self.next);
+        self.size +=1;
+        Ok(())
     }
 
     pub fn clear(&mut self) {
-        todo!()
+        self.buffer = vec![None; self.buffer.capacity()];
+        self.next = 0;
+        self.size = 0;
+        self.read = 0;
     }
 
     pub fn is_empty(&self) -> bool {
-        todo!()
+        self.size == 0
     }
 
     pub fn is_full(&self) -> bool {
-        todo!()
+        self.size == self.buffer.capacity()
+    }
+    
+    fn alter_next_index(&mut self, index: usize) -> usize {
+        if index + 1 < self.buffer.capacity() {
+            index + 1
+        }
+
+        else {
+            (index + 1) % self.buffer.capacity()
+        }
     }
 }
